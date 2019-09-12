4d17843aa9ad59644be0d4364b66ed6507043442
Nisha Joshi <nisha.joshi@sweetli.me>
2019-09-13 00:15:19 +0530

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import {Card} from 'react-native-elements';
+import React, {Component} from 'react';
+import {Card, Button, Icon} from 'react-native-elements';
 import {View, Text} from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
-function Contact(props) {
+class Contact extends Component{
 
-  return(
-    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>                
-    <Card
-    title = "Contact Us" >
-    <Text style={{margin:10}}>121, Clear Water Bay Road</Text>
-    <Text style={{margin:10}}>Clear Water Bay, Kowloon</Text>
-    <Text style={{margin:10}}>HONG KONG</Text>
-    <Text style={{margin:10}}>Tel: +852 1234 5678</Text>
-    <Text style={{margin:10}}>Fax: +852 8765 4321</Text>
-    <Text style={{margin:10}}>Email:confusion@food.net</Text>
-    </Card>
-    </Animatable.View>
-  )
+  sendMail() {
+    MailComposer.composeAsync({
+        recipients: ['confusion@food.net'],
+        subject: 'Enquiry',
+        body: 'To whom it may concern:'
+    })
+  }
+
+  render(){
+
+    return(
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>                
+      <Card
+      title = "Contact Us" >
+      <Text style={{margin:10}}>121, Clear Water Bay Road</Text>
+      <Text style={{margin:10}}>Clear Water Bay, Kowloon</Text>
+      <Text style={{margin:10}}>HONG KONG</Text>
+      <Text style={{margin:10}}>Tel: +852 1234 5678</Text>
+      <Text style={{margin:10}}>Fax: +852 8765 4321</Text>
+      <Text style={{margin:10}}>Email:confusion@food.net</Text>
+      <Button
+          title="Send Email"
+          buttonStyle={{backgroundColor: "#512DA8"}}
+          icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+          onPress={this.sendMail}
+          />
+      </Card>
+      </Animatable.View>
+    )
+  }
 }
+
 
 export default Contact;
